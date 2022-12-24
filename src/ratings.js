228cd4e7b7be7cc53d5f300573255cd69dafe08d
Scott Clingan <sclingan1@gmail.com>
2022-12-24 00:15:09 -0600

    refactoring typescript for HTML form

--- src/ratings.js
+++ src/ratings.js
@@ -1,32 +1,38 @@
 "use strict";
-/* Prevent default from form */
 exports.__esModule = true;
-exports.preventFormDefault = void 0;
+/* Prevent default from form */
 function preventFormDefault() {
     var form = document.getElementById('form-rating');
     if (form != null) {
         form.addEventListener('submit', (function (event) { return event.preventDefault(); }));
     }
 }
-exports.preventFormDefault = preventFormDefault;
 preventFormDefault();
 /* Listen for click on buttons, get innerHTML for rating */
 var ratingOutput = document.getElementById('rating--output');
 var buttonRating = document.getElementsByClassName('button--rating');
 for (var i = 0; i < 5; i++) {
     buttonRating[i].addEventListener('click', function (e) {
-        ratingOutput.innerHTML = e.target.innerHTML;
+        if (ratingOutput != null) {
+            ratingOutput.innerHTML = e.target.innerHTML;
+        }
     });
 }
 /* On submit hide rating card , display thank you card, animate */
 var submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', function () {
-    var ratingCard = document.getElementById('ratings-card');
-    var thankYou = document.getElementById('thank-you-card');
-    ratingCard.style.display = 'none';
-    thankYou.addEventListener('mouseover', function () {
-        thankYou.style.transform = 'rotateY(360deg)';
+if (submitButton != null) {
+    submitButton.addEventListener('click', function () {
+        var ratingCard = document.getElementById('ratings-card');
+        var thankYou = document.getElementById('thank-you-card');
+        if (ratingCard != null) {
+            ratingCard.style.display = 'none';
+        }
+        ;
+        thankYou.addEventListener('mouseover', function () {
+            thankYou.style.transform = 'rotateY(360deg)';
+        });
+        thankYou.style.display = 'block';
     });
-    thankYou.style.display = 'block';
-});
+}
+;
 exports["default"] = {};
--- src/ratings.ts
+++ src/ratings.ts
@@ -1,6 +1,5 @@
 /* Prevent default from form */
-
-export function preventFormDefault () {
+function preventFormDefault () {
     let form: HTMLElement | null = document.getElementById('form-rating');
     if(form != null) {
         form.addEventListener('submit', (event => event.preventDefault()));
@@ -10,27 +9,31 @@ export function preventFormDefault () {
 preventFormDefault();
 
 /* Listen for click on buttons, get innerHTML for rating */
-let ratingOutput: any = document.getElementById('rating--output');
+let ratingOutput: HTMLElement| null = document.getElementById('rating--output');
 let buttonRating = document.getElementsByClassName('button--rating') as HTMLCollectionOf<HTMLElement>;
 for(let i = 0; i < 5; i++){
 buttonRating[i].addEventListener('click', (e: any) => {
+    if(ratingOutput != null) {
     ratingOutput.innerHTML = e.target.innerHTML;
+    }
 });
 }
 
 /* On submit hide rating card , display thank you card, animate */
-
-
-let submitButton: any = document.getElementById('submit');
+let submitButton: HTMLElement | null = document.getElementById('submit');
+if(submitButton != null) {
 submitButton.addEventListener('click', function() {
-    let ratingCard: any = document.getElementById('ratings-card');
+    let ratingCard: HTMLElement | null = document.getElementById('ratings-card');
     let thankYou: any = document.getElementById('thank-you-card');
+    if(ratingCard != null) {
     ratingCard.style.display = 'none';
+    };
     thankYou.addEventListener('mouseover', function() {
         thankYou.style.transform = 'rotateY(360deg)';
     })
     thankYou.style.display = 'block';
-});
+})
+};
 
 
 
